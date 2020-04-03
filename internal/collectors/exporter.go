94a3158baa74e3a1a7eb47482a4160a514ff5a43
Damien Grisonnet <dgrisonn@redhat.com>
2020-04-03 11:26:36 +0200

    exporter: instrument metrics endpoint
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

---
+++ internal/collectors/exporter.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2020 Red Hat, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package collectors
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// InstrumentMetricHandler is a middleware that wraps the provided http.Handler
+// to observe requests sent to the exporter.
+func InstrumentMetricHandler(registry *prometheus.Registry, handler http.Handler) http.Handler {
+	requestsTotal := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "kube_events_exporter_requests_total",
+		Help: "Total number of scrapes.",
+	}, []string{"code"})
+
+	requestsInFlight := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "kube_events_exporter_requests_in_flight",
+		Help: "Current number of scrapes being served.",
+	})
+
+	requestDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "kube_events_exporter_request_duration_seconds",
+		Help: "Duration of all scrapes.",
+	}, []string{"code"})
+
+	registry.MustRegister(
+		requestsTotal,
+		requestsInFlight,
+		requestDuration,
+	)
+
+	return promhttp.InstrumentHandlerDuration(
+		requestDuration.MustCurryWith(prometheus.Labels{"code": "200"}),
+		promhttp.InstrumentHandlerInFlight(requestsInFlight,
+			promhttp.InstrumentHandlerCounter(requestsTotal, handler),
+		),
+	)
+}
