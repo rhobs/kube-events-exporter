e6c6cd97dec2e8251854ea9c2efc7ab45a4444c2
Damien Grisonnet <dgrisonn@redhat.com>
2020-06-23 14:30:53 +0200

    test: add event framework
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

---
+++ test/framework/event.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2020 Red Hat, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package framework
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+// CreateEvent creates the given Event.
+func (f *Framework) CreateEvent(t *testing.T, event *v1.Event, ns string) *v1.Event {
+	event, err := f.KubeClient.CoreV1().Events(ns).Create(context.TODO(), event, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("create event %s: %v", event.Name, err)
+	}
+
+	t.Cleanup(func() {
+		err := f.DeleteEvent(event.Namespace, event.Name)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	return event
+}
+
+// DeleteEvent deletes the given Event.
+func (f *Framework) DeleteEvent(ns, name string) error {
+	err := f.KubeClient.CoreV1().Events(ns).Delete(context.TODO(), name, metav1.DeleteOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "delete event %s", name)
+	}
+	return nil
+}
+
+// GetEvent gets the given Event.
+func (f *Framework) GetEvent(ns, name string) (*v1.Event, error) {
+	event, err := f.KubeClient.CoreV1().Events(ns).Get(context.TODO(), name, metav1.GetOptions{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "get event %s", name)
+	}
+	return event, nil
+}
+
+// WaitUntilEventReady waits until given Event is ready.
+func (f *Framework) WaitUntilEventReady(ns, name string) error {
+	err := wait.Poll(time.Second, f.DefaultTimeout, func() (bool, error) {
+		_, err := f.GetEvent(ns, name)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				return false, nil
+			}
+			return false, err
+		}
+		return true, nil
+	})
+	if err != nil {
+		return errors.Wrapf(err, "event %s not ready", name)
+	}
+
+	return nil
+}
