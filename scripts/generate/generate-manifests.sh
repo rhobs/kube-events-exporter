ef652505617754029ff6415330110ab38cd0cc13
Damien Grisonnet <dgrisonn@redhat.com>
2020-09-01 17:24:54 +0200

    jsonnet: set image repo and version from env
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- scripts/generate/generate-manifests.sh
+++ scripts/generate/generate-manifests.sh
@@ -6,7 +6,6 @@ set -o pipefail
 # error on unset variables
 set -u
 
-
 # Make sure to use project tooling
 PATH="$(pwd)/tmp/bin:${PATH}"
 
@@ -15,9 +14,10 @@ rm -rf manifests
 mkdir manifests
 
 jsonnet_path="scripts/generate"
-
 # Calling gojsontoyaml is optional, but we would like to generate yaml, not json
-jsonnet -J "${jsonnet_path}/vendor" -m manifests "${jsonnet_path}/kube-events-exporter.jsonnet" | xargs -I{} sh -c 'cat {} | gojsontoyaml > {}.yaml' -- {}
+jsonnet -J "${jsonnet_path}/vendor" -m manifests "${jsonnet_path}/kube-events-exporter.jsonnet" \
+    --ext-str VERSION --ext-str IMAGE_REPO \
+    | xargs -I{} sh -c 'cat {} | gojsontoyaml > {}.yaml' -- {}
 
 # Make sure to remove json files
-find manifests -type f ! -name '*.yaml' -delete                                              
+find manifests -type f ! -name '*.yaml' -delete
